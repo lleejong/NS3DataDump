25374d4712e0a77452e10ac104a76fe8cd152e56
lleejong <lleejong@gmail.com>
2016-11-18 02:11:54 +0900

    initial Commit

--- shell_sim.sh
+++ shell_sim.sh
@@ -20,7 +20,7 @@ echo SIM_TIME=$SIM_TIME
 
 echo "TX_ERR=${args[0]} TX_DELAY=${args[1]} TX_JITTER=${args[2]}   RX_LOSS=${args[3]} RX_DELAY=${args[4]} RX_JITTER=${args[5]}"
 
-(cd /var/lib/dce-linux-dev/source/ns3-dce;
+(cd /var/lib/dce-linux-dev/source/ns-3-dce;
 ./waf --run "kupakupa  --TypeOfConnection=p --tcp_cc=cubic --ModeOperation=false \
 --udp_bw=155Mbps --SimuTime=$SIM_TIME \
 --user_bw_down=155Mbps --user_bw_up=155Mbps \
